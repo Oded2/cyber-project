d8502b6ea51c01c490627a66d9772938aa5ca86b
Oded Benamotz <obenamotz@gmail.com>
2024-11-26 21:52:19 +0200

    Homepage skeleton progress

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let img: string;
+	export let title: string;
+	export let description: string;
+</script>
+
+<div class="card h-full w-full">
+	<figure class="h-1/2">
+		<img src={img} alt={title} />
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">{title}</h2>
+		<p>{description}</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,68 +1,53 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
 	import { hrefs } from '$lib';
+	import Card from '$lib/components/Card.svelte';
+	import creativity from '$lib/images/Creativity.svg';
+	import calendar from '$lib/images/Calendar.svg';
+	import map from '$lib/images/Map.svg';
 
-	const imgUrl = '/images/wallpaper.jpg';
+	const imgUrl =
+		'https://raw.githubusercontent.com/Oded2/Aerologger/refs/heads/main/src/lib/images/logo_simplified.png';
 	const description =
 		'A flight logging site like no other. Log past flights, and plan for future flights.';
 </script>
 
-<div class="hero min-h-screen" style="background-image: url({imgUrl});">
-	<div class="hero-overlay bg-opacity-40"></div>
-	<div class="hero-content text-center text-neutral-content">
-		<div class=" max-w-xl">
-			<h1 class="mb-5 text-5xl font-bold">Welcome to Aerologger</h1>
-			<p class="mb-5">
-				{description}
-			</p>
-			<a href={hrefs.signup} class="btn btn-primary">Get Started</a>
-			<button
-				on:click={() => {
-					document.getElementById('test')?.scrollIntoView({ behavior: 'smooth' });
-				}}
-				class="btn btn-secondary">Learn More</button
-			>
+<main class="py-10">
+	<div class="hero mb-10 bg-base-200">
+		<div class="hero-content flex-col lg:flex-row-reverse">
+			<img alt="Logo" src={imgUrl} class="max-w-sm rounded-lg shadow-2xl" />
+			<div>
+				<h1 class="text-5xl font-bold">Elevate Your Logbook!</h1>
+				<p class="py-6">
+					{description}
+				</p>
+				<a href={hrefs.signup} class="btn btn-primary">Get Started</a>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="hero min-h-screen" id="test">
-	<div class="carousel rounded-box">
-		<div class="carousel-item">hello world</div>
-		<div class="carousel-item">
-			<img
-				src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-				alt="Burger"
-			/>
+	<Container>
+		<div class="grid grid-cols-3 place-items-center">
+			<div class="col-auto h-full w-full">
+				<Card
+					img={creativity}
+					title="Log your flights, fast and easy."
+					description="With Aerologger, you can log your flights in a very simple and quick process. Aerologger's technology will then get as much information about your flight as it can, such as weather, flight route, and more!"
+				></Card>
+			</div>
+			<div class="col-auto h-full w-full">
+				<Card
+					img={calendar}
+					title="Stay updated."
+					description="Have a flight in two days? No problem. Simply plan a future flight and your pilot's calendar will update. Want to know your past flights? They will show up there as well."
+				></Card>
+			</div>
+			<div class="col-auto h-full w-full">
+				<Card
+					img={map}
+					title="Plan future flights."
+					description="This isn't just a logbook. Aerologger is also a flight planning application. Given your future takeoff time & date, Aerologger will automatically add it to your calendar, plan a route for you based on weather and airspace restrictions, and alert you of any possible delays."
+				></Card>
+			</div>
 		</div>
-		<div class="carousel-item">
-			<img
-				src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-				alt="Burger"
-			/>
-		</div>
-		<div class="carousel-item">
-			<img
-				src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
-				alt="Burger"
-			/>
-		</div>
-		<div class="carousel-item">
-			<img
-				src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
-				alt="Burger"
-			/>
-		</div>
-		<div class="carousel-item">
-			<img
-				src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
-				alt="Burger"
-			/>
-		</div>
-		<div class="carousel-item">
-			<img
-				src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
-				alt="Burger"
-			/>
-		</div>
-	</div>
-</div>
+	</Container>
+</main>
