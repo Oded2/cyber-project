b29525f934715711a0a7942e8062789e47dbf0df
Oded Benamotz <obenamotz@gmail.com>
2024-12-03 20:51:16 +0200

    Throws error instead of redirecting to non-existing page

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,4 +1,4 @@
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 import type { Actions } from './$types';
 import { hrefs } from '$lib';
@@ -13,10 +13,10 @@ export const actions: Actions = {
 		const password = formData.get('password') as string;
 		const displayName = formData.get('display') as string;
 		const username = formData.get('username') as string;
-		const { data, error } = await supabase.auth.signUp({ email, password });
-		if (error) {
-			console.error(error);
-			redirect(303, '/auth/error');
+		const { data, error: e } = await supabase.auth.signUp({ email, password });
+		if (e) {
+			console.error(e);
+			error(500, "Auth Error");
 		}
 		await supabase.from('profiles').insert([{ id: data.user?.id, display: displayName, username }]);
 		redirect(303, hrefs.signupSucess);
@@ -25,10 +25,10 @@ export const actions: Actions = {
 		const formData = await request.formData();
 		const email = formData.get('email') as string;
 		const password = formData.get('password') as string;
-		const { error } = await supabase.auth.signInWithPassword({ email, password });
-		if (error) {
-			console.error(error);
-			redirect(303, '/auth/error');
+		const { error: e } = await supabase.auth.signInWithPassword({ email, password });
+		if (e) {
+			console.error(e);
+			error(500, "Auth Error");
 		} else {
 			redirect(303, hrefs.home);
 		}
