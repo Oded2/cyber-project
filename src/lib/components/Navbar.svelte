fa59a39f5400afa157f540408ee2e89f6c00c6fd
Oded <obenamotz@gmail.com>
2024-12-04 17:03:41 +0200

    Fixed menu not working on safari

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -59,11 +59,13 @@
 						/>
 					</div>
 				</div>
+				<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
 				<ul
+					tabindex="0"
 					class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
 				>
 					<li>
-						<a href="temp" class="justify-between"> Profile </a>
+						<a href="/temp"> Profile </a>
 					</li>
 					<li><a href="temp">Settings</a></li>
 					<li><button on:click>Sign Out</button></li>
