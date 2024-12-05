983d90be6cf588aaf4fd93584ffb7e2a843432ec
Oded Benamotz <obenamotz@gmail.com>
2024-12-05 18:51:12 +0200

    Changed protected layout to server side code

--- src/routes/protected/+layout.server.ts
+++ src/routes/protected/+layout.server.ts
@@ -5,8 +5,7 @@ type Profile = {
     username: string;
 }
 
-export async function load({ parent }) {
-    const { user, supabase } = await parent();
+export async function load({ locals: { supabase, user } }) {
     const { data } = await supabase.from("profiles").select().eq("id", user?.id)
     return { profile: (data!)[0] as Profile }
 
