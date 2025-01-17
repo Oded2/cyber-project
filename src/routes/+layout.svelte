afe3dd60a485853068e4936888d181144fe344d4
Oded Benamotz <obenamotz@gmail.com>
2025-01-17 15:47:17 +0200

    Removed navbar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
-	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { error } from '@sveltejs/kit';
 	import { defaultProfilePicture, hrefs } from '$lib';
+	import { page } from '$app/stores';
+	import Dropdown from '$lib/components/Dropdown.svelte';
 
 	const { children, data } = $props();
 	// possible issue
 	const { supabase, session, user, profile } = data;
+
+	const pageData = $page;
+	let status = $derived(pageData.status);
+	let pathName = $derived(pageData.url.pathname);
+
+	const navItems = [
+		{ href: hrefs.contact, title: 'Contact' },
+		{ href: hrefs.search, title: 'Search' },
+		{ href: '/temp', title: 'Development' }
+	];
 
 	onMount(() => {
 		const { data } = supabase.auth.onAuthStateChange((event, newSession) => {
@@ -22,15 +33,78 @@
 
 		return () => data.subscription.unsubscribe();
 	});
-</script>
 
-<Navbar
-	profileImage={profile.image.length > 0 ? profile.image : defaultProfilePicture}
-	loggedIn={user ? true : false}
-	signOut={async () => {
+	async function handleSignOut(): Promise<void> {
 		const { error: e } = await supabase.auth.signOut();
 		if (e) error(e.status ?? 400, { message: e.message });
-	}}
-></Navbar>
+	}
+</script>
+
+{#if status == 200}
+	<div class="navbar bg-base-100">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<div tabindex="0" role="button" class="btn btn-ghost sm:hidden">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h8m-8 6h16"
+						/>
+					</svg>
+				</div>
+				<ul
+					class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
+				>
+					{#each navItems as item}
+						<li><a href={item.href}>{item.title}</a></li>
+					{/each}
+				</ul>
+			</div>
+			<a href={hrefs.home} class="btn btn-ghost text-xl">Aerologger</a>
+		</div>
+		<div class="navbar-center hidden sm:flex">
+			<ul class="menu menu-horizontal">
+				{#each navItems as item}
+					<li><a href={item.href}>{item.title}</a></li>
+				{/each}
+			</ul>
+		</div>
+		<div class="navbar-end">
+			{#if user}
+				<div class="dropdown dropdown-end">
+					<div tabindex="0" role="button" class="avatar btn btn-circle btn-ghost">
+						<div class="w-10 rounded-full">
+							<img alt="Profile" src={profile.image} id="profilePicture" />
+						</div>
+					</div>
+					<Dropdown>
+						<li>
+							<a href="/temp"> Profile </a>
+						</li>
+						<li><a href={hrefs.settings}>Settings</a></li>
+						<li><button onclick={handleSignOut}>Sign Out</button></li>
+					</Dropdown>
+				</div>
+			{:else if !pathName.includes(hrefs.login)}
+				<div class="join">
+					<div class="join-item me-2">
+						<a href={hrefs.login} class="btn btn-secondary">Log In</a>
+					</div>
+					<div class="join-item">
+						<a href={hrefs.signup} class="btn btn-primary">Sign Up</a>
+					</div>
+				</div>
+			{/if}
+		</div>
+	</div>
+{/if}
 
 {@render children()}
