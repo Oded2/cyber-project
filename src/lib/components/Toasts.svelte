63b9d236b144d0628977bf87e5d7f29099cc2850
Oded2 <obenamotz@gmail.com>
2025-02-08 19:12:13 +0200

    Minor change

--- src/lib/components/Toasts.svelte
+++ src/lib/components/Toasts.svelte
@@ -2,12 +2,10 @@
 	import { flip } from 'svelte/animate';
 	import { toasts, dismissToast } from '../toasts';
 	import Toast from './Toast.svelte';
-
-	const arr = $derived($toasts);
 </script>
 
 <section class="fixed bottom-2 left-2 flex flex-col gap-4 print:hidden">
-	{#each arr as toast (toast.id)}
+	{#each $toasts as toast (toast.id)}
 		<div animate:flip={{ duration: 200 }}>
 			<Toast type={toast.type} duration={toast.duration} handleClose={() => dismissToast(toast.id)}>
 				{toast.text}
