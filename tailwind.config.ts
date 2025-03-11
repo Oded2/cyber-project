f45179532d2ba40a8af60a1ddff0afb53b7b6691
Oded2 <--global>
2025-03-11 09:33:11 +0200

    Upgrade to DaisyUI 5 & TailwindCSS 4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,9 @@
 import type { Config } from 'tailwindcss';
-import daisyui from 'daisyui';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {
-
-		}
-	}, daisyui: {
-		themes: [
-			{
-				mytheme: {
-
-					"primary": "#2563eb",
-
-					"secondary": "#6b7280",
-
-					"accent": "#34d399",
-
-					"neutral": "#090909",
-
-					"base-100": "#f3f4f6",
-
-					"info": "#0f766e",
-
-					"success": "#00a67e",
-
-					"warning": "#ffa800",
-
-					"error": "#ff0039",
-				},
-			},
-		],
-	},
-
-	plugins: [require("@tailwindcss/typography"), daisyui]
+		extend: {}
+	}
 } satisfies Config;
