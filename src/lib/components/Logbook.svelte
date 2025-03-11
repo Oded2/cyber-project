6e8cfd803b9835a7263478d73009c39520cc8eab
Oded2 <--global>
2025-03-11 09:10:06 +0200

    Added handling for 0 logs

--- src/lib/components/Logbook.svelte
+++ src/lib/components/Logbook.svelte
@@ -22,29 +22,33 @@
 	let showEntries: boolean = $state(defaultShowEntries);
 </script>
 
-<div class="flex items-center gap-4">
-	<div class="join">
-		<button
-			class="btn join-item"
-			class:btn-active={showEntries}
-			onclick={() => (showEntries = true)}
-			aria-label="Logbook"
-		>
-			<i class="fa-solid fa-book"></i>
-		</button>
-		<button
-			class="btn join-item"
-			class:btn-active={!showEntries}
-			onclick={() => (showEntries = false)}
-			aria-label="Calendar"
-		>
-			<i class="fa-solid fa-calendar-days"></i>
-		</button>
+{#if logs.length > 0}
+	<div class="flex items-center gap-4">
+		<div class="join">
+			<button
+				class="btn join-item"
+				class:btn-active={showEntries}
+				onclick={() => (showEntries = true)}
+				aria-label="Logbook"
+			>
+				<i class="fa-solid fa-book"></i>
+			</button>
+			<button
+				class="btn join-item"
+				class:btn-active={!showEntries}
+				onclick={() => (showEntries = false)}
+				aria-label="Calendar"
+			>
+				<i class="fa-solid fa-calendar-days"></i>
+			</button>
+		</div>
+		<h1 class="mt-5 border-b-2 pb-2 text-xl font-bold">{title}</h1>
 	</div>
-	<h1 class="mt-5 border-b-2 pb-2 text-xl font-bold">{title}</h1>
-</div>
-{#if showEntries}
-	<LogEntries originalLogs={logs} {aircrafts} {supabase} {edit}></LogEntries>
+	{#if showEntries}
+		<LogEntries originalLogs={logs} {aircrafts} {supabase} {edit}></LogEntries>
+	{:else}
+		<Calendar originalLogs={logs} {supabase} {edit}></Calendar>
+	{/if}
 {:else}
-	<Calendar originalLogs={logs} {supabase} {edit}></Calendar>
+	<h1>No logs here!</h1>
 {/if}
