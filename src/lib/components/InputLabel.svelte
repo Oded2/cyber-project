24ef54c6a41d86e4d77430c4746fc9560edc94a9
Oded2 <--global>
2025-03-11 09:46:52 +0200

    Added Input Label component

---
+++ src/lib/components/InputLabel.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	const { children }: { children: Snippet } = $props();
+</script>
+
+<label class="input input-ghost rounded-md border-1 border-gray-300! font-medium outline-hidden!">
+	{@render children()}
+</label>
